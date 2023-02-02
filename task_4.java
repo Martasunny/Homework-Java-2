9a50ec1212bbde78ec0a368fdcfea52be4f988a6
Marta <Martasunnyy@gmail.com>
2023-02-02 19:52:54 +0300

    task 4

---
+++ task_4.java
@@ -0,0 +1,17 @@
+//Дано два числа, например 3 и 56, необходимо составить следующие строки:
+//3 + 56 = 59 3 – 56 = -53 3 * 56 = 168 Используем метод StringBuilder.append().
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder = StringBuilder1 = createSomeString(3, 56);
+        System.out.println(StringBuilder1);
+    }
+private static StringBuilder createSomeString(int a, int b){
+    StringBuilder result = new StringBuilder();
+    result.append(a).append("+").append(b).append("=").append(a + b).append("\n")
+    .append(a).append("-").append(b).append("=").append(a - b).append("\n")
+    .append(a).append("*").append(b).append("=").append(a * b).append("\n")
+    return result;
+}
+
+
+}
